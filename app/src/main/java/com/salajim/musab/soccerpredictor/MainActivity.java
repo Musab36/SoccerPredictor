026887763cd98415f6948e14570b83b66b08c4d5
Musab36 <musabhd@gmail.com>
2018-04-17 11:39:39 +0300

    Binding the views with butterKnife and onclick listener

--- app/src/main/java/com/salajim/musab/soccerpredictor/MainActivity.java
+++ app/src/main/java/com/salajim/musab/soccerpredictor/MainActivity.java
@@ -1,13 +1,38 @@
 package com.salajim.musab.soccerpredictor;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    public static final String TAG = MainActivity.class.getSimpleName();
+    @Bind(R.id.totalGames) EditText mTotalGames;
+    @Bind({R.id.homeWins}) EditText mHomeWins;
+    @Bind(R.id.awayLoses) EditText mAwayLoses;
+    @Bind(R.id.draws) EditText mDraws;
+    @Bind(R.id.awayHomeWins) EditText mAwayHomeWins;
+    @Bind(R.id.homeAwayLoses) EditText mHomeAwayLoses;
+    @Bind(R.id.submitBtn) Button mSubmitBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mSubmitBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        getPrediction();
+    }
+
+    private void getPrediction() {
     }
 }
